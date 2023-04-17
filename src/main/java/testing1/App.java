85a24805fbf81edd97918deec295c74379312653
ironshadowz <chenwl911@gmail.com>
2023-04-17 14:42:47 +0800

    Up to slide 17

--- src/main/java/testing1/App.java
+++ src/main/java/testing1/App.java
@@ -1,13 +1,36 @@
 package testing1;
 
+import java.io.Console;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Console con = System.console();
+        // Ask for name
+        String name = con.readLine("What is your name?");
+        if (name.length() > 0) {
+            System.out.printf("\nNice to meet you, %s", name);
+            Integer age = 80;
+            System.out.printf("\n%s is %d years old.", name, age);
+        } else {
+            System.out.println("You have not told me your name");
+        }
+// slide 17
+        String input = con.readLine("\nWhat is your hobby?");
+        input.trim();
+        if (input.equals("swim"))
+        {
+            System.out.println("\nYou float");
+        }else if (input.equals("cycle")){
+            System.out.println("\nYou roll");
+        } else if (input.equals("jog")){
+            System.out.println("\nYour heart is strong");
+        } else {
+            System.out.print("\nNothing");
+        }
     }
 }
